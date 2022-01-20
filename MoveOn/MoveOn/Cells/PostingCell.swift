7b6b5cd37157ccea36f82df067462a74b5dd917b
jumi-shim <jumi6827@naver.com>
2022-01-20 13:09:31 +0900

    readme 업데이트

--- MoveOn/MoveOn/Cells/PostingCell.swift
+++ MoveOn/MoveOn/Cells/PostingCell.swift
@@ -14,6 +14,7 @@ class PostingCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
     }
     
     func setData(data:PostModel){
